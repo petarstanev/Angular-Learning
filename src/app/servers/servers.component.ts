abb45da1b923b72b79430cc941debc707b2193e3
Petar Stanev <PetarStanev@tcb365.onmicrosoft.com>
2019-03-01 15:13:44 +0000

    Added ngfor directive.

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,6 +10,7 @@ export class ServersComponent implements OnInit {
   serverCreationStatus = 'No server was created!';
   serverName = 'Test Server';
   serverCreated = false;
+  servers = ['TestServer', 'TestServer 2'];
 
   username = '';
 
@@ -23,6 +24,7 @@ export class ServersComponent implements OnInit {
 
   onCreateServer(){
     this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = "Server was created. Name is " + this.serverName;
   }
 
